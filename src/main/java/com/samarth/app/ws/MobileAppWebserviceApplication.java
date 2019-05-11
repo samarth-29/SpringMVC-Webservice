3bb18625cb13413cbc52cff5fb8023b2dd1dcda4
samarth <samarth29@github.com>
2019-05-11 10:55:55 -0700

    first commit

---
+++ src/main/java/com/samarth/app/ws/MobileAppWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.samarth.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWebserviceApplication.class, args);
+	}
+
+}
