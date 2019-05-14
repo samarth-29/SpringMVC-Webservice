760435802ca8d1f32ad8af728a4cc85f60b22d8d
samarth <samarth29@github.com>
2019-05-14 13:46:48 -0700

    Added custom exception handling

---
+++ src/main/java/com/samarth/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,13 @@
+package com.samarth.app.ws.exceptions;
+
+public class UserServiceException extends RuntimeException {
+
+	private static final long serialVersionUID = 617724359818839592L;
+	public UserServiceException(String message)
+	{
+		super(message);
+	}
+	
+	
+
+}
