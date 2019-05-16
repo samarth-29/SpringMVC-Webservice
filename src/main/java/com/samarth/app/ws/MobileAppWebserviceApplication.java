844aeaa7eae484b47534daadac109b96c1508735
samarth <samarth29@github.com>
2019-05-16 15:50:55 -0700

    add context-path

--- src/main/java/com/samarth/app/ws/MobileAppWebserviceApplication.java
+++ src/main/java/com/samarth/app/ws/MobileAppWebserviceApplication.java
@@ -2,14 +2,22 @@ package com.samarth.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.samarth.app.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWebserviceApplication {
+public class MobileAppWebserviceApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWebserviceApplication.class);
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWebserviceApplication.class, args);
 	}
