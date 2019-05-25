4b5b68290ff3d6336210a603e2a68f847b95f625
samarth <samarth29@github.com>
2019-05-25 13:05:43 -0700

    provided h2 in memory database support

--- src/main/java/com/samarth/app/ws/security/SecurityConstants.java
+++ src/main/java/com/samarth/app/ws/security/SecurityConstants.java
@@ -11,7 +11,7 @@ public class SecurityConstants {
     public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
     public static final String PASSWORD_RESET_REQUEST_URL = "/users/password-reset-request";
     public static final String PASSWORD_RESET_URL = "/users/password-reset";
-    //public static final String TOKEN_SECRET = "jf9i4jgu83nfl0";
+    public static final String H2_CONSOLE = "/h2-console/**";
     
     public static String getTokenSecret()
     {
